f853803695827fa5a7a1c00d40f331c4615fa72e
Xiphiar <me@xiphiar.com>
2023-06-20 17:58:25 -0400

    Validate backend env

--- apps/backend/src/utils/validateEnv.ts
+++ apps/backend/src/utils/validateEnv.ts
@@ -4,6 +4,13 @@ const validateEnv = () => {
   cleanEnv(process.env, {
     NODE_ENV: str(),
     PORT: port(),
+    DB_HOST: str(),
+    DB_PORT: str(),
+    DB_DATABASE: str(),
+    SECRET_KEY: str(),
+    LOG_FORMAT: str(),
+    LOG_DIR: str(),
+    ORIGIN: str(),
   });
 };
 
