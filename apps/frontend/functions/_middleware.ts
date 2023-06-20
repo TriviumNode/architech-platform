f203ab6a82c2a21cbb638d1fea7e173741980102
Xiphiar <me@xiphiar.com>
2023-06-20 16:41:23 -0400

    Cloudflare API proxy

---
+++ apps/frontend/functions/_middleware.ts
@@ -0,0 +1,27 @@
+import proxyflare from "@flaregun-net/proxyflare-for-pages"
+import { Route } from "@flaregun-net/proxyflare-for-pages/build/types"
+
+const apiRoute: Route = {
+    from: { pattern: "alpha.architech.zone/api/*" },
+    to: { url: "api.architech.zone" },
+}
+
+const apiRoute2: Route = {
+    from: { pattern: "alpha.architech.zone/example" },
+    to: { url: "example.com" },
+}
+  
+const routes: Route[] = [apiRoute, apiRoute2]
+
+// `PagesFunction` is from @cloudflare/workers-types
+export const onRequest: PagesFunction[] = [
+  (context) =>
+    proxyflare({
+      config: {
+        global: { debug: true },
+        routes,
+      },
+    })(context),
+  // other Pages plugins and middleware
+]
+
