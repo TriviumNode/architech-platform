d2ddae75e71a0a210c6245cab3e9668af3728b90
Xiphiar <me@xiphiar.com>
2023-06-21 20:17:12 -0400

    Update addresses.ts

--- packages/architech-lib/src/addresses.ts
+++ packages/architech-lib/src/addresses.ts
@@ -1,14 +1,14 @@
 // Constantine-3
-// export const MARKETPLACE_ADDRESS = 'archway1kxhr3gkldhxhq6j5j8npc2kn0jpv7e306dzlj35j53k4njcdjlpslpmuqa';
-// export const COLLECTION_FACTORY_ADDRESS = '';
-// export const CREDIT_ADDRESS = 'archway1ghuqw74theqpnh7mqdykdleltqvcgqhmcmw9asmxl3qp78spk5hqx29xmk';
+export const MARKETPLACE_ADDRESS = 'archway1kxhr3gkldhxhq6j5j8npc2kn0jpv7e306dzlj35j53k4njcdjlpslpmuqa';
+export const COLLECTION_FACTORY_ADDRESS = '';
+export const CREDIT_ADDRESS = 'archway1ghuqw74theqpnh7mqdykdleltqvcgqhmcmw9asmxl3qp78spk5hqx29xmk';
 
-// export const CW721_CODE_ID = 219;
+export const CW721_CODE_ID = 219;
 
 
 // Local Testnet
-export const MARKETPLACE_ADDRESS = 'archway1k9m3n9288tpkv6tfe9k5avzwx0n208500mu3udttdftmlmfmwqzqrhjdde';
-export const COLLECTION_FACTORY_ADDRESS = '';
-export const CREDIT_ADDRESS = 'archway19ju9qdz5e0hsmsf0589v6zfqs4uqh82xe37ussjkh7pk0xyggzgs2kajgz';
+// export const MARKETPLACE_ADDRESS = 'archway1k9m3n9288tpkv6tfe9k5avzwx0n208500mu3udttdftmlmfmwqzqrhjdde';
+// export const COLLECTION_FACTORY_ADDRESS = '';
+// export const CREDIT_ADDRESS = 'archway19ju9qdz5e0hsmsf0589v6zfqs4uqh82xe37ussjkh7pk0xyggzgs2kajgz';
 
-export const CW721_CODE_ID = 312;
+// export const CW721_CODE_ID = 312;
