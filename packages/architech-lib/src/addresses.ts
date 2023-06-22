dde8a836f0dc3d6d10c33262ccb6a59dc3e18b74
Xiphiar <me@xiphiar.com>
2023-06-22 03:18:58 -0400

    Update addresses.ts

--- packages/architech-lib/src/addresses.ts
+++ packages/architech-lib/src/addresses.ts
@@ -1,7 +1,7 @@
 // Constantine-3
-export const MARKETPLACE_ADDRESS = 'archway1xxjlujxa3lsj75tuea66mduf73d92q0edkn7f3nmhsqc7gda8j2qxhzmap';
+export const MARKETPLACE_ADDRESS = 'archway1ucx4cqgpgxn9yhwz0senqkt67vev3q6lvyq56zse08y3lza2g5wsuaukzp';
 export const COLLECTION_FACTORY_ADDRESS = '';
-export const CREDIT_ADDRESS = 'archway1da7nwsshgcxvejm53vda75rxquh6lenmxf95euwkx9mj76g75txs6jte2v';
+export const CREDIT_ADDRESS = 'archway16p73xs5u5zusmukpejsnyjfzffqwlqltwqwmthy4ta9twuyxm7cqgy3s03';
 
 export const CW721_CODE_ID = 219;
 
