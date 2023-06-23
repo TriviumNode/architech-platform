9f1a804d2e87367f85711ba1e216d2baa6acf4df
Xiphiar <me@xiphiar.com>
2023-06-23 14:17:27 -0400

    Update addresses.ts

--- packages/architech-lib/src/addresses.ts
+++ packages/architech-lib/src/addresses.ts
@@ -1,5 +1,5 @@
 // Constantine-3
-export const MARKETPLACE_ADDRESS = 'archway1xllczwp7ktvept63daygw7e5a063sy28uuu9cd4rlu95h96gue9q4qltrq';
+export const MARKETPLACE_ADDRESS = 'archway1wsg28uurld6ftrrnwgjq53uywgg4y8q6dksmycmcvk7r55s5337s5k24m5';
 export const COLLECTION_FACTORY_ADDRESS = '';
 export const CREDIT_ADDRESS = 'archway16p73xs5u5zusmukpejsnyjfzffqwlqltwqwmthy4ta9twuyxm7cqgy3s03';
 
@@ -7,8 +7,8 @@ export const CW721_CODE_ID = 219;
 
 
 // Local Testnet
-// export const MARKETPLACE_ADDRESS = 'archway1urq9husv0qsn4gsnf6gs2ma3s9uwzetgjzgk8c49sndg9vr6j6lsnlnetq';
+// export const MARKETPLACE_ADDRESS = 'archway1am4k4klp68wafrk32m7jhh23gd02x6x2exk5v6sprmhl2jqveydsll4rsd';
 // export const COLLECTION_FACTORY_ADDRESS = '';
-// export const CREDIT_ADDRESS = 'archway10z6kz6p7pk5pdyqpuefukxvvqcfv9sayw9sdpspn3mduydqpawss3jgqfx';
+// export const CREDIT_ADDRESS = 'archway17yyusgrfvxt3ehudkz9khgnktegu86zdg4ytra25k5zv8ccjc5mqu0qpvd';
 
 // export const CW721_CODE_ID = 312;
