5ac13f69ebfcb4e97b0225fa68156c5c01d07e02
Xiphiar <me@xiphiar.com>
2023-07-04 21:31:57 -0400

    more debug

--- apps/backend/src/controllers/index.controller.ts
+++ apps/backend/src/controllers/index.controller.ts
@@ -1,9 +1,10 @@
+import { MARKETPLACE_ADDRESS, CREDIT_ADDRESS, CW721_CODE_ID, CHAIN_ID } from '@/config';
 import { NextFunction, Request, Response } from 'express';
 
 class IndexController {
   public index = (req: Request, res: Response, next: NextFunction) => {
     try {
-      res.sendStatus(200);
+      res.status(200).json({ config: { CHAIN_ID, MARKETPLACE_ADDRESS, CREDIT_ADDRESS, CW721_CODE_ID } });
     } catch (error) {
       next(error);
     }
