f9f09f4769f77539116dfb493e59d40a80fafc57
Xiphiar <me@xiphiar.com>
2023-08-11 19:28:51 -0400

    update factory address

--- packages/architech-lib/src/addresses.ts
+++ packages/architech-lib/src/addresses.ts
@@ -13,7 +13,7 @@ export const getAddresses = (chainId: string) => {
             return {
                 CREDIT_ADDRESS: 'archway1xmlcasxa8xs9saz2avk9t4pcjyf6h94skhwdhm6vsqrsvhmvq96qnxvhku',
                 MARKETPLACE_ADDRESS: 'archway13f8fv36qm87kaj3hfl9fspyv2p6jusvu92z59z3f0vgs975xjy4qwdg4cg',
-                NFT_FACTORY_ADDRESS: 'archway19wlwrrflvklmy8jj8dm3ssekn5x97ahqsttr4fq5y4540xskgjaqwjadzh',
+                NFT_FACTORY_ADDRESS: 'archway147e5025c282fuy2v4an08px47ay8972hq2z9lqddkt5yc9msjvjqgs5aer',
                 CW721_CODE_ID: 219,
                 ARCHID_ADDRESS: 'archway1lr8rstt40s697hqpedv2nvt27f4cuccqwvly9gnvuszxmcevrlns60xw4r',
             };
