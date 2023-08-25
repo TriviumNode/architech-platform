ebd4c711596403144fa4b0bc5d07a261202679ad
Xiphiar <me@xiphiar.com>
2023-08-25 13:16:32 -0400

    add admin fee query

--- apps/frontend/src/Pages/Admin/MinterQueries.tsx
+++ apps/frontend/src/Pages/Admin/MinterQueries.tsx
@@ -1,20 +1,16 @@
-import { ADMINS, CATEGORIES, getConfig } from "@architech/lib";
+import { getConfig, getMintStatus, getPrice } from "@architech/lib";
 import { FC, ReactElement, useState } from "react";
 import { Col } from "react-bootstrap";
 import { toast } from "react-toastify";
-import MultiSelect from "../../Components/MultiSelect";
-//@ts-expect-error
-import { Switch } from 'react-switch-input';
+import {Buffer} from 'buffer';
 
 import styles from './create.module.scss'
-import { purgeCollection, purgeTokens, refreshCollection } from "../../Utils/backend";
-import ConnectWallet from "../../Components/ConnectWallet";
 import { useUser } from "../../Contexts/UserContext";
 import SmallLoader from "../../Components/SmallLoader";
 import { QueryClient } from "../../Utils/queryClient";
 import ReactJson from "react-json-view";
 
-type LOADING = 'config'
+type LOADING = 'config' | 'price' | 'adminfee'
 const AdminMinterQueries: FC<{}> = (): ReactElement => {
     const { user } = useUser()
     const [minterAddress, setMinterAddress] = useState('');
@@ -36,6 +32,57 @@ const AdminMinterQueries: FC<{}> = (): ReactElement => {
         setLoading(undefined);
     }
 
+    const handleGetPrice = async (e: any) => {
+      e.preventDefault();
+
+      setLoading('price');
+      try {
+          const result = await getPrice({client: QueryClient, contract: minterAddress});
+          console.log(result);
+          setData(result)
+      } catch (err: any) {
+          console.error(err)
+          toast.error(err.toString());
+      }
+      setLoading(undefined);
+    }
+
+    const handleGetMintStatus = async (e: any) => {
+      e.preventDefault();
+
+      setLoading('price');
+      try {
+          const result = await getMintStatus({client: QueryClient, contract: minterAddress});
+          console.log(result);
+          setData(result)
+      } catch (err: any) {
+          console.error(err)
+          toast.error(err.toString());
+      }
+      setLoading(undefined);
+    }
+
+    const handleGetAdminFee = async (e: any) => {
+      e.preventDefault();
+
+      setLoading('adminfee');
+      try {
+        const rawData = await QueryClient.queryContractRaw(minterAddress, new Uint8Array(Buffer.from('fee')))
+        if (!rawData) throw new Error('No data found at key `fee`')
+        const string: string = Buffer.from(rawData).toString()
+        const obj = JSON.parse(string);
+
+        console.log(obj);
+        setData(obj)
+      } catch (err: any) {
+          console.error(err)
+          toast.error(err.toString());
+      }
+      setLoading(undefined);
+    }
+
+
+
 
     return (
         <div style={{margin: '48px'}} className='d-flex flex-column'>
@@ -55,6 +102,7 @@ const AdminMinterQueries: FC<{}> = (): ReactElement => {
             </form>
             <div className='d-flex gap8 justify-content-center mb24'>
                 <button type='button' disabled={!!loading} onClick={handleGetConfig}>Query Config{loading==='config' && <>&nbsp;<SmallLoader /></>}</button>
+                <button type='button' disabled={!!loading} onClick={handleGetAdminFee}>Query Admin Fee{loading==='adminfee' && <>&nbsp;<SmallLoader /></>}</button>
                 {/* <button type='button' disabled={!!loading} onClick={handlePurgeTokens}>Purge Tokens{loading==='purgetokens' && <>&nbsp;<SmallLoader /></>}</button> */}
                 {/* <button type='button' disabled={!!loading} onClick={handlePurge} style={{background: 'red'}}>Purge Collection{loading==='purge' && <>&nbsp;<SmallLoader /></>}</button> */}
             </div>
